277b9cc1ef730577c4fc8bc64143484bff282549
Davinson Granada <118221608+Davinsongranada@users.noreply.github.com>
2025-12-02 16:28:13 -0500

    Nueva info

--- 09-tesloShop/src/app/auth/pages/login-page/login-page.ts
+++ 09-tesloShop/src/app/auth/pages/login-page/login-page.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -11,6 +12,9 @@ export class LoginPage {
   fb = inject(FormBuilder);
   hasError = signal(false);
   isPosting = signal(false);
+  router = inject(Router)
+
+  authService = inject(AuthService)
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -26,9 +30,20 @@ export class LoginPage {
       return;
     }
 
-    // const { email = '', password = '' } = this.loginForm.value;
+    const { email = '', password = '' } = this.loginForm.value;
 
-    console.log(this.loginForm.value);
+    //console.log(this.loginForm.value);
     // console.log({email, password});
+
+    this.authService.login(email!, password!).subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigateByUrl('/')
+        return
+      }
+      this.hasError.set(true)
+      setTimeout(() => {
+        this.hasError.set(false)
+      }, 2000);
+    })
   }
 }
